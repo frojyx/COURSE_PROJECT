1f1eecb3ad6f18b4a9cc8ffb462c92b1f7d525b7
Pavel <shmyginp05@gmail.com>
2025-11-25 19:38:16 +0300

    fix

--- TrackSorter.cpp
+++ TrackSorter.cpp
@@ -1,49 +1,72 @@
 #include "TrackSorter.h"
 #include "TrackRepository.h"
 #include "Track.h"
-#include <algorithm>
 
 TrackSorter::TrackSorter(TrackRepository& repository)
     : repository(repository)
 {
 }
 
+template <typename T, typename Compare>
+static void shellSort(QList<T>& list, Compare comp)
+{
+    int n = list.size();
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            T temp = list[i];
+            int j;
+            for (j = i; j >= gap && comp(temp, list[j - gap]); j -= gap) {
+                list[j] = list[j - gap];
+            }
+            list[j] = temp;
+        }
+    }
+}
+
 void TrackSorter::sortByTitle(bool ascending) {
     QList<Track> tracks = repository.findAllTracks();
-    auto comparator = [ascending](const Track& a, const Track& b) {
+
+    auto cmp = [ascending](const Track& a, const Track& b){
         return ascending ? a.getTitle() < b.getTitle()
                          : a.getTitle() > b.getTitle();
     };
-    std::stable_sort(tracks.begin(), tracks.end(), comparator);
+
+    shellSort(tracks, cmp);
     repository.setTracks(tracks);
 }
 
 void TrackSorter::sortByArtist(bool ascending) {
     QList<Track> tracks = repository.findAllTracks();
-    auto comparator = [ascending](const Track& a, const Track& b) {
+
+    auto cmp = [ascending](const Track& a, const Track& b){
         return ascending ? a.getArtist() < b.getArtist()
                          : a.getArtist() > b.getArtist();
     };
-    std::stable_sort(tracks.begin(), tracks.end(), comparator);
+
+    shellSort(tracks, cmp);
     repository.setTracks(tracks);
 }
 
 void TrackSorter::sortByYear(bool ascending) {
     QList<Track> tracks = repository.findAllTracks();
-    auto comparator = [ascending](const Track& a, const Track& b) {
+
+    auto cmp = [ascending](const Track& a, const Track& b){
         return ascending ? a.getYear() < b.getYear()
                          : a.getYear() > b.getYear();
     };
-    std::stable_sort(tracks.begin(), tracks.end(), comparator);
+
+    shellSort(tracks, cmp);
     repository.setTracks(tracks);
 }
 
 void TrackSorter::sortByDuration(bool ascending) {
     QList<Track> tracks = repository.findAllTracks();
-    auto comparator = [ascending](const Track& a, const Track& b) {
+
+    auto cmp = [ascending](const Track& a, const Track& b){
         return ascending ? a.getDuration() < b.getDuration()
                          : a.getDuration() > b.getDuration();
     };
-    std::stable_sort(tracks.begin(), tracks.end(), comparator);
+
+    shellSort(tracks, cmp);
     repository.setTracks(tracks);
 }
