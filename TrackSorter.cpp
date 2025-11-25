4f4059bb67ee31ad620c6d0cf19160c28886ab96
Pavel <shmyginp05@gmail.com>
2025-11-25 19:29:14 +0300

    fix

--- TrackSorter.cpp
+++ TrackSorter.cpp
@@ -1,4 +1,3 @@
-// TrackSorter.cpp
 #include "TrackSorter.h"
 #include "TrackRepository.h"
 #include "Track.h"
@@ -12,36 +11,39 @@ TrackSorter::TrackSorter(TrackRepository& repository)
 void TrackSorter::sortByTitle(bool ascending) {
     QList<Track> tracks = repository.findAllTracks();
     auto comparator = [ascending](const Track& a, const Track& b) {
-        return ascending ? a.getTitle() < b.getTitle() : a.getTitle() > b.getTitle();
+        return ascending ? a.getTitle() < b.getTitle()
+                         : a.getTitle() > b.getTitle();
     };
-    std::sort(tracks.begin(), tracks.end(), comparator);
+    std::stable_sort(tracks.begin(), tracks.end(), comparator);
     repository.setTracks(tracks);
 }
 
 void TrackSorter::sortByArtist(bool ascending) {
     QList<Track> tracks = repository.findAllTracks();
     auto comparator = [ascending](const Track& a, const Track& b) {
-        return ascending ? a.getArtist() < b.getArtist() : a.getArtist() > b.getArtist();
+        return ascending ? a.getArtist() < b.getArtist()
+                         : a.getArtist() > b.getArtist();
     };
-    std::sort(tracks.begin(), tracks.end(), comparator);
+    std::stable_sort(tracks.begin(), tracks.end(), comparator);
     repository.setTracks(tracks);
 }
 
 void TrackSorter::sortByYear(bool ascending) {
     QList<Track> tracks = repository.findAllTracks();
     auto comparator = [ascending](const Track& a, const Track& b) {
-        return ascending ? a.getYear() < b.getYear() : a.getYear() > b.getYear();
+        return ascending ? a.getYear() < b.getYear()
+                         : a.getYear() > b.getYear();
     };
-    std::sort(tracks.begin(), tracks.end(), comparator);
+    std::stable_sort(tracks.begin(), tracks.end(), comparator);
     repository.setTracks(tracks);
 }
 
 void TrackSorter::sortByDuration(bool ascending) {
     QList<Track> tracks = repository.findAllTracks();
     auto comparator = [ascending](const Track& a, const Track& b) {
-        return ascending ? a.getDuration() < b.getDuration() : a.getDuration() > b.getDuration();
+        return ascending ? a.getDuration() < b.getDuration()
+                         : a.getDuration() > b.getDuration();
     };
-    std::sort(tracks.begin(), tracks.end(), comparator);
+    std::stable_sort(tracks.begin(), tracks.end(), comparator);
     repository.setTracks(tracks);
 }
-
